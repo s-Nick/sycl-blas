1a06383cbcd67c178a1f4105495465062e030dd5
nscipione <nicolo.scipione@codeplay.com>
2023-06-26 17:26:32 +0100

    [benchmark] Update omatadd_batch benchmark state counters

--- benchmark/syclblas/extension/omatadd_batched.cpp
+++ benchmark/syclblas/extension/omatadd_batched.cpp
@@ -25,24 +25,28 @@
 
 #include "../../../test/unittest/extension/extension_reference.hpp"
 #include "../utils.hpp"
-#include "common/common_utils.hpp"
-#include "interface/extension_interface.h"
 
 template <typename scalar_t>
 std::string get_name(std::string ts_a, std::string ts_b, int m, int n,
                      scalar_t alpha, scalar_t beta, index_t lda_mul,
-                     index_t ldb_mul, index_t ldc_mul) {
+                     index_t ldb_mul, index_t ldc_mul, index_t stride_a_mul,
+                     index_t stride_b_mul, index_t stride_c_mul,
+                     index_t batch_size) {
   std::ostringstream str{};
   str << "BM_omatadd_batch<" << blas_benchmark::utils::get_type_name<scalar_t>()
       << ">/" << ts_a << "/" << ts_b << "/" << m << "/" << n << "/" << alpha
-      << "/" << beta << "/" << lda_mul << "/" << ldb_mul << "/" << ldc_mul;
+      << "/" << beta << "/" << lda_mul << "/" << ldb_mul << "/" << ldc_mul
+      << "/" << stride_a_mul << "/" << stride_b_mul << "/" << stride_c_mul
+      << "/" << batch_size;
   return str.str();
 }
 
 template <typename scalar_t>
 void run(benchmark::State& state, blas::SB_Handle* sb_handle_ptr, int ti_a,
-         int ti_b, index_t m, index_t n, index_t lda_mul, index_t ldb_mul,
-         index_t ldc_mul, scalar_t alpha, scalar_t beta, bool* success) {
+         int ti_b, index_t m, index_t n, scalar_t alpha, scalar_t beta,
+         index_t lda_mul, index_t ldb_mul, index_t ldc_mul,
+         index_t stride_a_mul, index_t stride_b_mul, index_t stride_c_mul,
+         index_t batch_size, bool* success) {
   // initiliaze the state label
   blas_benchmark::utils::set_benchmark_label<scalar_t>(
       state, sb_handle_ptr->get_queue());
@@ -59,25 +63,18 @@ void run(benchmark::State& state, blas::SB_Handle* sb_handle_ptr, int ti_a,
   const auto ldb = (*t_str_b == 't') ? ldb_mul * n : ldb_mul * m;
   const auto ldc = ldc_mul * n;
 
-  const auto size_a = lda * ((*t_str_a == 't') ? m : n);
-  const auto size_b = ldb * ((*t_str_b == 't') ? m : n);
-  const auto size_c = ldc * n;
+  const auto size_a = lda * ((*t_str_a == 't') ? m : n) * batch_size;
+  const auto size_b = ldb * ((*t_str_b == 't') ? m : n) * batch_size;
+  const auto size_c = ldc * n * batch_size;
 
-  const auto stride_a = size_a;
-  const auto stride_b = size_b;
-  const auto stride_c = size_c;
-  const auto batch_size = static_cast<index_t>(1);
+  const auto stride_a = size_a * stride_a_mul;
+  const auto stride_b = size_b * stride_b_mul;
+  const auto stride_c = size_c * stride_c_mul;
 
-  blas_benchmark::utils::init_level_1_counters<
-      blas_benchmark::utils::Level1Op::copy, scalar_t>(state, 3 * m * n);
-
-  state.counters["n_fl_ops"] = 3 * static_cast<double>(m * n);
-  state.counters["lda_m"] = (double)lda_mul;
-  state.counters["ldb_m"] = (double)ldb_mul;
-  state.counters["trans_a"] = (double)((*t_str_a == 't') ? 1 : 0);
-  state.counters["trans_b"] = (double)((*t_str_b == 't') ? 1 : 0);
-  state.counters["m"] = (double)m;
-  state.counters["n"] = (double)n;
+  blas_benchmark::utils::init_extension_counters<
+      blas_benchmark::utils::ExtensionOP::omatadd_batch, scalar_t>(
+      state, t_str_a, t_str_b, m, n, lda_mul, ldb_mul, ldc_mul, stride_a_mul,
+      stride_b_mul, stride_c_mul, batch_size);
 
   blas::SB_Handle& sb_handle = *sb_handle_ptr;
 
@@ -157,30 +154,36 @@ void run(benchmark::State& state, blas::SB_Handle* sb_handle_ptr, int ti_a,
 template <typename scalar_t>
 void register_benchmark(blas_benchmark::Args& args,
                         blas::SB_Handle* sb_handle_ptr, bool* success) {
-  auto omatadd_params =
-      blas_benchmark::utils::get_omatadd_params<scalar_t>(args);
+  auto omatadd_batch_params =
+      blas_benchmark::utils::get_omatadd_batch_params<scalar_t>(args);
 
-  for (auto p : omatadd_params) {
+  for (auto p : omatadd_batch_params) {
     std::string ts_a, ts_b;
-    index_t m, n, lda_mul, ldb_mul, ldc_mul;
+    index_t m, n, lda_mul, ldb_mul, ldc_mul, stride_a_mul, stride_b_mul,
+        stride_c_mul, batch_size;
     scalar_t alpha, beta;
-    std::tie(ts_a, ts_b, m, n, alpha, beta, lda_mul, ldb_mul, ldc_mul) = p;
+    std::tie(ts_a, ts_b, m, n, alpha, beta, lda_mul, ldb_mul, ldc_mul,
+             stride_a_mul, stride_b_mul, stride_c_mul, batch_size) = p;
     int t_a = static_cast<int>(blas_benchmark::utils::to_transpose_enum(ts_a));
     int t_b = static_cast<int>(blas_benchmark::utils::to_transpose_enum(ts_b));
 
     auto BM_lambda = [&](benchmark::State& st, blas::SB_Handle* sb_handle_ptr,
                          int t_a, int t_b, index_t m, index_t n, scalar_t alpha,
                          scalar_t beta, index_t lda_mul, index_t ldb_mul,
-                         index_t ldc_mul, bool* success) {
+                         index_t ldc_mul, index_t stride_a_mul,
+                         index_t stride_b_mul, index_t stride_c_mul,
+                         index_t batch_size, bool* success) {
       run<scalar_t>(st, sb_handle_ptr, t_a, t_b, m, n, alpha, beta, lda_mul,
-                    ldb_mul, ldc_mul, success);
+                    ldb_mul, ldc_mul, stride_a_mul, stride_b_mul, stride_c_mul,
+                    batch_size, success);
     };
     benchmark::RegisterBenchmark(
         get_name<scalar_t>(ts_a, ts_b, m, n, alpha, beta, lda_mul, ldb_mul,
-                           ldc_mul)
+                           ldc_mul, stride_a_mul, stride_b_mul, stride_c_mul,
+                           batch_size)
             .c_str(),
         BM_lambda, sb_handle_ptr, t_a, t_b, m, n, alpha, beta, lda_mul, ldb_mul,
-        ldc_mul, success)
+        ldc_mul, stride_a_mul, stride_b_mul, stride_c_mul, batch_size, success)
         ->UseRealTime();
   }
 }
